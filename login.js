7de622fddb535cc0e3bd45c9344f0a9dad0306c7
AbhishekDeshpande1909 <43665056+AbhishekDeshpande1909@users.noreply.github.com>
2020-08-16 12:07:24 +0530

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-function nameCome(abc)
-{
-    
-    let cloned = document.querySelector('#Emptydiv').cloneNode(true);
-    cloned.innerHTML=abc;
-  
-    let comments = document.querySelector('#login-main');
-    comments.insertBefore(cloned,comments.children[1])
-}
